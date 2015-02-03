a4e5a51f26667732019afce2962b218a7e731872
2084246S <rhiannascott_13@hotmail.com>
2015-02-03 20:13:35 +0000

    Chapter 8 -completed

--- tango/rango/views.py
+++ tango/rango/views.py
@@ -3,6 +3,48 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 
+from rango.forms import CategoryForm
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print form.errors
+    else:
+        form = CategoryForm()
+    return render(request, 'rango/add_category.html', {'form': form})
+
+from rango.forms import PageForm
+
+def add_page(request, category_name_slug):
+
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+                cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                # probably better to use a redirect here.
+                return category(request, category_name_slug)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    context_dict = {'form':form, 'category': cat}
+
+    return render(request, 'rango/add_page.html', context_dict)
+
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     views_list =  Page.objects.order_by('-views')[:5]
@@ -13,14 +55,13 @@ def about(request):
     context_dict = {'boldmessage':"This tutorial was made by Rhianna Scott, 2084246S" }
     return render(request,'rango/about.html',context_dict)
 
-def category(request, category_name_slug):
 
-    # Create a context dictionary which we can pass to the template rendering engine.
+
+def category(request, category_name_slug):
+    context = RequestContext(request)
+    category_name = decode_url(category_name_slug)
     context_dict = {}
     try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # So the .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
         context_dict['category_name'] = category.name
 
@@ -34,9 +75,6 @@ def category(request, category_name_slug):
         # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
     except Category.DoesNotExist:
-        # We get here if we didn't find the specified category.
-        # Don't do anything - the template displays the "no category" message for us.
-        pass
 
-    # Go render the response and return it to the client.
+        pass
     return render(request, 'rango/category.html', context_dict)
