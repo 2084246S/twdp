265b6f1039ef9cee93d4281ced1b5d0efd4d2ff5
2084246S <rhiannascott_13@hotmail.com>
2015-02-21 18:17:53 +0000

    Chapter 12 -completed

--- tango/tango/urls.py
+++ tango/tango/urls.py
@@ -10,8 +10,14 @@ if not settings.DEBUG:
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^rango/', include('rango.urls')),
-
-)
+                       url(r'^accounts/', include('registration.backends.simple.urls')),
+                       url(r'^accounts/password_change/$',
+                           'django.contrib.auth.views.password_change',
+                           {'post_change_redirect' : '/accounts/password_change/done/$'},
+                           name="password_change_form"),
+                       url(r'^accounts/password_change/done/$',
+                        'django.contrib.auth.views.password_change_done',name='password_change_done'),
+                       )
 if settings.DEBUG:
     urlpatterns += patterns(
         'django.views.static',
