3a19fc7f5366988aa3b9abe6667b3b604349acdf
2084246S <2084246S@student.gla.ac.uk>
2015-03-06 12:27:56 +0000

    Chapter  16&17 - completed

--- tango/tango/urls.py
+++ tango/tango/urls.py
@@ -17,9 +17,9 @@ urlpatterns = patterns('',
                        (r'^accounts/', include('registration.backends.simple.urls')),
                        url(r'^accounts/password_change/$',
                            'django.contrib.auth.views.password_change',
-                           {'post_change_redirect': '/accounts/password_change/done/$'},
+                           {'post_change_redirect': '/accounts/password_change/done/'},
                            name="password_change_form"),
-                       url(r'^accounts/password_change/done/$',
+                       url(r'^accounts/password_change/done/',
                            'django.contrib.auth.views.password_change_done', name='password_change_done')
 )
 
